0fdfdefb4d6fae3ba61b6326721d5443fa049ede
2781816683@qq.com <2781816683@qq.com>
2017-12-27 13:33:28 +0800

    简单优化
    上传图片资源，优化关卡编辑器的使用

--- PushBoxEditorSource/PushBoxEditor.h
+++ PushBoxEditorSource/PushBoxEditor.h
@@ -1,13 +1,13 @@
-﻿#pragma once
-#include "MyWin32Util.h"
+#pragma once
+#include "..\Util\MyWin32Util.h"
 
 #define BACKGROUND 0
 #define WALL 1
-#define MAN 2
-#define BOX 3
-#define BLANK 4
-#define TARGET 5
-#define BOXONTARGET 6
+#define BOX 2
+#define TARGET 3
+#define MAN 4
+#define BOXONTARGET 5
+#define BLANK 6
 
 #define LM_FAIL_RESOURCE 0
 #define LM_FAIL_FILE 1
